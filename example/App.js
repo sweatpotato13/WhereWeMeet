c5bb1acf35ff2652c3b45a95127505b2b080275a
Handong Kim <aggsae@gmail.com>
2020-10-24 10:36:52 +0900

    feat: Implement makeMarker function, others
    - Marker를 반환하는 makeMarker 함수 작성
    - NaverMapView의 onMapClick에서 반환하는 e에 담긴 위, 경도 정보를
    저장하는 useState(localInfo)
    - localInfo에서 값을 받아 마커 생성

--- example/App.js
+++ example/App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import NaverMapView, {Circle, Marker, Path, Polyline, Polygon, Align} from "./map";
 import {PermissionsAndroid, Platform, Text, TouchableOpacity, View} from "react-native";
 import {NavigationContainer} from '@react-navigation/native';
@@ -28,22 +28,37 @@ const TextScreen = () => {
     return <Text>text</Text>
 }
 
+const makeMarker = (latitude, longitude) => {
+    return (
+        <Marker coordinate={{latitude, longitude}}
+                title='해당 영역 주소'
+                description='장소 세부 정보, 저장/삭제 버튼 구현(커스텀 마커)'/>
+    );
+}
+
 const MapViewScreen = ({navigation}) => {
     useEffect(() => {
         requestLocationPermission();
     }, []);
+
+    const [locaInfo, setLocaInfo] = useState({latitude: 37.5665, longitude: 126.87905});
 
     return <>
         <NaverMapView style={{width: '100%', height: '100%'}}
                       showsMyLocationButton={true}
                       onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
                       onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
-                      onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
+                      onMapClick={e => {
+                          console.warn('onMapClick', JSON.stringify(e))
+                          setLocaInfo({latitude: e.latitude, longitude: e.longitude});
+                        //   console.warn('asgashjkadshlksadh', JSON.stringify(locaInfo));
+                        }}
                       useTextureView>
-            <Marker 
-                coordinate={{latitude: 37.5665, longitude: 126.97905}}
-                title='this is marker'
-                description='this is a marker example' />
+            {/* 클릭 시 마커 표현, 조건에 따라 마커 저장 
+                handleOnMapClick -> 위, 경도 정보 저장
+            */}
+            {/* {makeMarker(37.5665, 126.97905)} */}
+            {makeMarker(locaInfo.latitude, locaInfo.longitude)}
         </NaverMapView>
         <TouchableOpacity style={{position: 'absolute', bottom: '10%', right: 8}} onPress={() => navigation.navigate('stack')}>
             <View style={{backgroundColor: 'gray', padding: 4}}>
