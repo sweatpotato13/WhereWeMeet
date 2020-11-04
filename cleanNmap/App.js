7dbeed56f6e723032df89bfa5744a8ef0d0ecbde
Handong Kim <aggsae@gmail.com>
2020-11-04 15:19:30 +0900

    fix: Delete comment
    - 컴포넌트를 return 하는 구분 내에 주석 작성 시 syntax 에러 발생
    - TODO를 명시했던 구문 삭제

--- cleanNmap/App.js
+++ cleanNmap/App.js
@@ -21,7 +21,6 @@ const App = () => {
   return <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name='home' component={HomeScreen}/>
-      {/* TODO :: 이 사이에 무언가 마커에 대한 정보를 제공하기 위한 tab이 제공되어야 할 것 같다. flag 조건에 따라? 아이디어는 생각 중 */}
       <Stack.Screen name='stack' component={MapViewScreen}/>
     </Stack.Navigator>
   </NavigationContainer>
@@ -30,7 +29,7 @@ const App = () => {
 const HomeScreen = () =>
   <Tab.Navigator>
     <Tab.Screen name={'map'} component={MapViewScreen}/>
-    <Tab.Screen name={'text'} component={TextScreen}/> {/* CHECK :: 이 탭의 용도는? */}
+    <Tab.Screen name={'text'} component={TextScreen}/>
   </Tab.Navigator>
 
 const TextScreen = () => <Text>text</Text>
